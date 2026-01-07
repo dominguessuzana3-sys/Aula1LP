b393894a058f633ed7c64f54eb6500b39d0cb712
Suzana <dominguessuzana3@gmail.com>
2026-01-07 17:54:54 -0300

    funções parametros

--- funções.js
+++ funções.js
@@ -7,3 +7,26 @@ function nome(){
     console.log ("Bem vinda ao curso de Lógica de Programação!");
 }
 nome();
+
+
+function gererNumeroAleatorio(){
+    console.log (Math.random());
+}
+    
+gererNumeroAleatorio();
+
+function gerarnumeroAleatorio(){
+    return Math.random();
+}
+    let numeroGerado = gerarnumeroAleatorio();
+console.log ("O numero gerado foi: " + numeroGerado);
+
+function dataAtual(){
+    const data = new Date();
+    return data.toDateString();
+}
+const data = dataAtual();
+console.log ("A data atual é: " + data);
+
+
+
